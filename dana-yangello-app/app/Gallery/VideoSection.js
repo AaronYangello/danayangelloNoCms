6ae327677f0221a5ea979b4facf5e24478875e49
Aaron Yangello <ayangello@gmail.com>
2025-07-22 22:22:35 -0400

    gallery page finished

--- dana-yangello-app/app/Gallery/VideoSection.js
+++ dana-yangello-app/app/Gallery/VideoSection.js
@@ -8,9 +8,13 @@ import VideoItem from '@/app/Gallery/VideoItem';
 export default function VideoSection({ sectionTitle, videos }) {
     const maxVideosPerRow = 3;
     const letters = sectionTitle.split('');
+    const rows = [];
+    for (let i = 0; i < videos.length; i += maxVideosPerRow) {
+        rows.push(videos.slice(i, i + maxVideosPerRow));
+    }
     return (
         <div className="bg-peach relative min-h-[400px] py-8 flex items-center">
-            <div className="absolute absolute top-0 bottom-0 left-1/2 -translate-x-1/2 md:left-8 md:-translate-x-0 flex flex-row md:flex-col justify-center md:justify-between pointer-events-none py-16 w-[80%] mx-auto mb-16 md:mb-0">
+            <div className="absolute inset-y-0 left-1/2 -translate-x-1/2 md:left-8 md:translate-x-0 flex flex-row md:flex-col justify-center md:justify-between pointer-events-none py-16 mb-16 md:mb-0 items-center">
                 {letters.map((char, i) => (
                     <span
                         key={i}
@@ -20,24 +24,22 @@ export default function VideoSection({ sectionTitle, videos }) {
                     </span>
                 ))}
             </div>
-            <div className="mx-auto my-16 m:my-0 w-[80%] h-full flex flex-col items-center">
-                {videos.map((video, index) => (
-                    index % maxVideosPerRow === 0 && (
-                        <div
-                            key={index}
-                            className="w-full flex flex-col md:flex-row gap-4 md:gap-16 items-center"
-                        >
-                            {videos.slice(index, index + maxVideosPerRow).map((video, subIndex) => (
-                                <VideoItem
-                                    key={subIndex}
-                                    thumbnail={video.thumbnail}
-                                    alt={video.description}
-                                    videoUrl={video.videoUrl}
-                                    className="flex-1 aspect-video"
-                                />
-                            ))}
-                        </div>
-                    )     
+            <div className="mx-auto mt-20 md:mt-0 w-[80%] flex flex-col items-center">
+                {rows.map((row, index) => (
+                    <div
+                        key={index}
+                        className={`w-full flex md:justify-evenly flex-col md:flex-row gap-4 md:gap-16 items-center ${index > 0 ? 'mt-4 md:mt-16' : ''}`}
+                    >
+                        {row.map((video, subIndex) => (
+                            <VideoItem
+                                key={subIndex}
+                                thumbnail={video.thumbnail}
+                                alt={video.description}
+                                videoUrl={video.videoUrl}
+                                className="w-[90%] md:w-[29%] aspect-video"
+                            />
+                        ))}
+                    </div>
                 ))}
             </div>
         </div>
