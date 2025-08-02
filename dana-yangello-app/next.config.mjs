bbcc9fbf5d4f894380832840e2f4ebed8433168d
Aaron Yangello <ayangello@gmail.com>
2025-08-01 21:30:37 -0400

    updated next.config.mjs to work with vercel

--- dana-yangello-app/next.config.mjs
+++ dana-yangello-app/next.config.mjs
@@ -1,16 +1,3 @@
-import { defineConfig } from 'next'
-
-/** @type {import('next').NextConfig} */
-export default defineConfig({
+export default {
   reactStrictMode: true,
-  swcMinify: true,
-
-  // opt into the new App Router and server‐action support
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
-
-  // you can still define any rewrites / redirects here
-  // rewrites: async () => [ ... ],
-})
+}
